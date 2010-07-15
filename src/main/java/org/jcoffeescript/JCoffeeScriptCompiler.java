df86bd0d4c9950bd61937876bdc7702a2ce69692
Daniel Cassidy <mail@danielcassidy.me.uk>
2010-07-15 18:02:00 +0100

    Rewrite JCoffeeScriptCompiler.java to solve a number of problems:
    1. Use proper Rhino embedding API instead of wrapping Rhino Shell.
    2. As a side-effect of (1), ensure that coffee-script compilation errors are passed back to the caller instead of being ignored. At the moment this just means letting JavaScriptExceptions fall through to the caller, but we could implement a better way later.
    3. Ensure that Streams and Readers are always closed.
    4. Always exit the Rhino Context after we've finished with it.
    5. Don't unnecessarily re-initialize standard JavaScript objects for every compilation.
    6. Specify UTF-8 character encoding for coffee-script.js instead of assuming platform-encoding. It probably doesn't matter since coffee-script.js seems to contain only ASCII characters, but at least this way we will always get consistent behaviour across platforms.
    7. Remove misuse of InputStream.ready(), which would cause incorrect behaviour if the system happened to block while reading coffee-script.js. This will 'probably' never happen, but let's avoid bizarre intermittent bugs by fixing it anyway.
    
    Most of these problems don't matter when using the command line JCoffeeScript, but I plan to implement a JCoffeeScript Ant task for which these changes are more important. They will also be helpful to anyone who plans to embed JCoffeeScript as part of a larger program, especially if they use Rhino elsewhere in the same program.

--- src/main/java/org/jcoffeescript/JCoffeeScriptCompiler.java
+++ src/main/java/org/jcoffeescript/JCoffeeScriptCompiler.java
@@ -17,51 +17,56 @@
 package org.jcoffeescript;
 
 import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Script;
 import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.ScriptableObject;
-import org.mozilla.javascript.tools.shell.Main;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.UnsupportedEncodingException;
 
 public class JCoffeeScriptCompiler {
-    private Context context;
-    private ScriptableObject global;
-    private Script compileScript;
+
+    private Scriptable globalScope;
 
     public JCoffeeScriptCompiler() {
-        context = Context.enter();
-        context.setOptimizationLevel(-1);
-        global = context.initStandardObjects();
-        loadCoffeeScriptCompiler(context, global);
-        compileScript = Main.loadScriptFromSource(context, "CoffeeScript.compile(this.coffeeScriptSource);", "compile command", 1, null);
-    }
-
-    private void loadCoffeeScriptCompiler(Context context, ScriptableObject global) {
-        final StringBuilder source = new StringBuilder(300000);
-        readCoffeeScriptJsInto(source);
-        final Script script = Main.loadScriptFromSource(context, source.toString(), "<cmd>", 1, null);
-        Main.evaluateScript(script, context, global);
-    }
-
-    public String compile(String coffeeScriptSource) {
-        final Scriptable compilationContext = context.initStandardObjects(global);
-        compilationContext.put("coffeeScriptSource", compilationContext, coffeeScriptSource);
-        return (String) Main.evaluateScript(compileScript, context, global);
-    }
-
-    private void readCoffeeScriptJsInto(StringBuilder stringBuilder) {
-        final InputStream stream = getClass().getClassLoader().getResourceAsStream("org/jcoffeescript/coffee-script.js");
-        Reader reader = new InputStreamReader(stream);
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("org/jcoffeescript/coffee-script.js");
         try {
-            while (reader.ready()) {
-                stringBuilder.append((char) reader.read());
+            try {
+                Reader reader = new InputStreamReader(inputStream, "UTF-8");
+                try {
+                    Context context = Context.enter();
+                    context.setOptimizationLevel(-1); // Without this, Rhino hits a 64K bytecode limit and fails
+                    try {
+                        globalScope = context.initStandardObjects();
+                        context.evaluateReader(globalScope, reader, "coffee-script.js", 0, null);
+                    } finally {
+                        Context.exit();
+                    }
+                } finally {
+                    reader.close();
+                }
+            } catch (UnsupportedEncodingException e) {
+                throw new Error(e); // This should never happen
+            } finally {
+                inputStream.close();
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new Error(e); // This should never happen
+        }
+    }
+
+    public String compile (String coffeeScriptSource) {
+        Context context = Context.enter();
+        try {
+            Scriptable compileScope = context.newObject(globalScope);
+            compileScope.setParentScope(globalScope);
+            compileScope.put("coffeeScriptSource", compileScope, coffeeScriptSource);
+            return (String)context.evaluateString(compileScope, "CoffeeScript.compile(coffeeScriptSource);",
+                    "JCoffeeScriptCompiler", 0, null);
+        } finally {
+            Context.exit();
         }
     }
 
